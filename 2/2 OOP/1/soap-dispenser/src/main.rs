a1f9b47da6c90a8cc26154d5c0cec2727a9ce16f
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2024-02-17 15:39:50 +0100

    finish rust

--- 2/2 OOP/1/soap-dispenser/src/main.rs
+++ 2/2 OOP/1/soap-dispenser/src/main.rs
@@ -1,3 +1,18 @@
+mod dispenser;
+use dispenser::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut dp = SoapDispenser::new(40, 2);
+
+    println!("{dp}");
+
+    dp.push();
+    dp.push();
+    dp.push();
+
+    println!("{dp}");
+
+    dp.refill();
+
+    println!("{dp}");
 }
