1c71f18367d1086b04317e5055824d6e9ce843e1
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2024-12-09 20:15:52 +0100

    laf

--- 3/3 Progtech/12/Arkanoid/Arkanoid.java
+++ 3/3 Progtech/12/Arkanoid/Arkanoid.java
@@ -5,6 +5,9 @@
  */
 package Arkanoid;
 
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+
 /**
  *
  * @author bli
@@ -15,6 +18,12 @@ public class Arkanoid {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+    try {
+      UIManager.setLookAndFeel(new FlatLightLaf());
+    } catch (Exception e) {
+      System.err.println("Failed to initialize FlatLaf");
+    }
+    
     ArkanoidGUI gui = new ArkanoidGUI();
   }
 
