d44c0d2eb3b076df29795aae506451aa11fbbac6
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-02-12 19:08:40 +0100

    asd

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   rules: {
     'no-console': 'off',
+    'curly': ['warn', 'multi-or-nest', 'consistent'],
   },
   ignores: ['**/vendor/**'],
   solid: true,
